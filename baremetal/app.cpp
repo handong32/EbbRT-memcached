380696db8c0844e164155a9daefb33aa9e10f0e2
Jim Cadden <jmcadden@bu.edu>
2014-05-07 20:59:32 -0400

    Adds listening message upon server bringup

--- baremetal/app.cpp
+++ baremetal/app.cpp
@@ -9,6 +9,7 @@ void AppMain()
   auto id = ebbrt::ebb_allocator->AllocateLocal();
   auto mc = ebbrt::EbbRef<ebbrt::Memcached>(id);
   mc->StartListening(11211);
+  ebbrt::kprintf("Memcached server listening on 11211\n");
 #if __EBBRT_ENABLE_TRACE__
   ebbrt::trace::Init();
 #endif
